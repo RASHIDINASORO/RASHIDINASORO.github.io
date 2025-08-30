2287c9ebf04f0b05ae8dc154b58bd29b90bff8c0
RASHIDINASORO <rashidinasoro912@gmail.com>
2025-08-30 17:11:26 +0100

    Updates

--- assets/index-CG1Vki3Q.js
+++ assets/index-CG1Vki3Q.js
@@ -1,4 +1,4 @@
-import{g as bn,R as Te}from"./index-_pm8TSTL.js";/*!
+import{g as bn,R as Te}from"./index-BchsD_3P.js";/*!
  * Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  * Copyright 2024 Fonticons, Inc.
