1d67ff0754026880844b7fb283bbfc4e9784088c
RASHIDINASORO <rashidinasoro912@gmail.com>
2025-08-30 15:35:22 +0100

    again

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,18 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Set the base path to '/' for a user/organization site
-  base: "/",
-  plugins: [react(), tailwindcss()],
-  server: {
-    open: true,
-    host: false,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        secure: true,
-      },
+    // Set the base path to '/' for a user/organization site
+    base: "/",
+    plugins: [react(), tailwindcss()],
+    server: {
+        open: true,
+        host: false,
+        proxy: {
+            "/api": {
+                target: "http://localhost:5000",
+                changeOrigin: true,
+                secure: true,
+            },
+        },
     },
-  },
 });
