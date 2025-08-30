60cafc859c72e0c5f8178daa81707f4589f812ba
RASHIDINASORO <rashidinasoro912@gmail.com>
2025-08-30 15:11:24 +0100

    now

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,21 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  // Conditionally set the base path based on the environment variable
-  const base = env.VITE_REPO_NAME ? `/${env.VITE_REPO_NAME}/` : "/";
-
-  return {
-    base, // Use the corrected base path
-    plugins: [react(), tailwindcss()],
-    server: {
-      open: true,
-      host: false,
-      proxy: {
-        "/api": {
-          target: "http://localhost:5000",
-          changeOrigin: true,
-          secure: true,
-        },
+// https://vitejs.dev/config/
+export default defineConfig({
+  // Set the base path to '/' for a user/organization site
+  base: "/",
+  plugins: [react(), tailwindcss()],
+  server: {
+    open: true,
+    host: false,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: true,
       },
     },
-  };
+  },
 });
